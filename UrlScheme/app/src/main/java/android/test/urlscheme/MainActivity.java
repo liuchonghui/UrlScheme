d086583ecf7954f5b4f345c6547f08a09dab14c2
liuchonghui <liuchonghui@xiaomi.com>
2018-05-24 09:56:43 +0800

    commit changes.
    N/A
    
    Signed-off-by: liuchonghui <liuchonghui@xiaomi.com>

---
+++ UrlScheme/app/src/main/java/android/test/urlscheme/MainActivity.java
@@ -0,0 +1,20 @@
+package android.test.urlscheme;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
