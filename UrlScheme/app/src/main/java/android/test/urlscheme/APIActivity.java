79d966388a0d46e8f238f57d7b3a59c9f5d3bc7c
liuchonghui <liuchonghui@xiaomi.com>
2018-05-25 09:49:04 +0800

    commit changes.
    N/A
    
    Signed-off-by: liuchonghui <liuchonghui@xiaomi.com>

--- UrlScheme/app/src/main/java/android/test/urlscheme/APIActivity.java
+++ UrlScheme/app/src/main/java/android/test/urlscheme/APIActivity.java
@@ -1,9 +1,12 @@
 package android.test.urlscheme;
 
+import android.compact.utils.IntentCompactUtil;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
+import android.util.Log;
 import android.widget.TextView;
 
 public class APIActivity extends AppCompatActivity {
@@ -13,7 +16,15 @@ public class APIActivity extends AppCompatActivity {
         setContentView(R.layout.activity_api);
         final TextView content = (TextView) findViewById(R.id.content);
         if (getIntent() != null) {
+            String strIntent = IntentCompactUtil.convertIntentToString(getIntent());
+            Log.d("PPP", getClass().getSimpleName() + "|strIntent|" + strIntent);
             String text = getIntent().getStringExtra(Intent.EXTRA_TEXT);
+            if (TextUtils.isEmpty(text)) {
+                Uri uri = getIntent().getData();
+                if (uri != null) {
+                    text = uri.getQueryParameter(Intent.EXTRA_TEXT);
+                }
+            }
             if (!TextUtils.isEmpty(text)) {
                 content.setText("调起并接收到StringExtra：" + text);
             }
